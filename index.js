bed979bd77ccfe1ae1e24101548eb09f7b3324a0
Carlos Alexandre Leutz <54986289+Carlos-Alexandre-Leutz@users.noreply.github.com>
2021-03-04 16:16:23 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+ testes = servicoPlanodeContas.getGrid()
+                            testes.then(function(v) {
+                            console.log('aqui')
+                            console.log(v[0]);
+});
