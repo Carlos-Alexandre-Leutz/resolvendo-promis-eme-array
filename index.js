3ff8afb2638d2640944ded53a039d8ce9581f534
Carlos Alexandre Leutz <54986289+Carlos-Alexandre-Leutz@users.noreply.github.com>
2021-10-01 13:52:00 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,33 @@
                             console.log('aqui')
                             console.log(v[0]);
 });
+
+
+   const obterProdutoDoFornecedorPorId = async (
+            idForncedor,
+            idProduto
+        ) => {
+            if (!idForncedor) {
+                idForncedor = 1;
+            }
+            let retorno;
+            await get(idForncedor).then((resp) => {
+                if (resp.status == 200) {
+                    if (resp.data) {
+                        resp.data.produtos.forEach((index) => {
+                            if (index.produtoID == idProduto) {
+                                retorno = index;
+                            }
+                        });
+                    }
+                }
+            });
+            return retorno;
+        };
+        async function get(idForncedor) {
+            let retorno = await requisicoes.getPorId(
+                "produtosFornecedor",
+                idForncedor
+            );
+            return retorno;
+        }
